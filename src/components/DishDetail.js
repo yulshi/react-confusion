42bebc2cfebda68ee60c2ed3a8cbb3741518ea2d
Yulong Shi <yu.long.shi@oracle.com>
2019-03-18 12:37:33 +0800

    init commit

---
+++ src/components/DishDetail.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const dish = this.props.dish;
+        if (dish) {
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        } else {
+            return (
+                <div></div>
+            )
+        }
+    }
+}
+
+export default DishDetail;
